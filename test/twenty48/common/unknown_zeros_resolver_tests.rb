19251ab53d14e9ebedd0aa3955c3bb52bd1e188e
John Lees-Miller <jdleesmiller@gmail.com>
2016-12-27 18:00:37 +0000

    Add test that shows 3-step resolution is inexact

--- test/twenty48/common/unknown_zeros_resolver_tests.rb
+++ test/twenty48/common/unknown_zeros_resolver_tests.rb
@@ -313,5 +313,42 @@ module Twenty48
         0, 2, 1, 2
       ])
     end
+
+    def test_exact_resolve_depth_3x3_to_8
+      #
+      # If we start from this state:
+      # 0, 0, 0,
+      # 0, 0, 1,
+      # 1, 0, 2
+      # and go left, we get
+      # ?, ?, ?
+      # 1, ?, ?
+      # 1, 2, ?
+      # Now, no matter what, we can win in two more moves (down and then left).
+      # However, if the middle cell (for example) is 2^2, we can actually win in
+      # one more move (down). This example imposes a limit on how many states
+      # deep we can resolve and still have an exact value function. In
+      # particular, we can only resolve exactly 2 moves ahead.
+      #
+      state = [
+        0, 0, 0,
+        0, 0, 1,
+        1, 0, 2
+      ]
+
+      resolver_0 = make_resolver(3, 3, 0)
+      assert_nil moves_to_win(resolver_0, state)
+
+      resolver_1 = make_resolver(3, 3, 1)
+      assert_nil moves_to_win(resolver_1, state)
+
+      resolver_2 = make_resolver(3, 3, 2)
+      assert_nil moves_to_win(resolver_2, state)
+
+      # It's possible (but not certain), that we will win in 2 moves, even
+      # though we'll definitely win in 3.
+      resolver_3 = make_resolver(3, 3, 3)
+      assert_equal 3, moves_to_win(resolver_3, state)
+    end
   end
 end
