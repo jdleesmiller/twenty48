36d3c81aa75ee4ac5e632e6b0f6176ff91cd8f92
John Lees-Miller <jdleesmiller@gmail.com>
2017-01-26 22:25:31 +0000

    Make vbyte encoding work on large files

--- ext/twenty48/test.cpp
+++ ext/twenty48/test.cpp
@@ -1,51 +1,56 @@
 #include <fstream>
 #include <iostream>
+#include <sys/stat.h>
 #include <vector>
 
 #include "vbyte.h"
 
+#define BIN_IN_FILE  "0222.bin"
+#define VBYTE_FILE   "0222.vbyte"
+#define BIN_OUT_FILE "0222.vbyte.bin"
+
+size_t count_records_in_file(const char *pathname, size_t record_size) {
+  struct stat stat_buf;
+  int rc = stat(pathname, &stat_buf);
+  if (rc != 0) return 0;
+
+  off_t file_size = stat_buf.st_size;
+  if (file_size % record_size != 0) {
+    throw std::invalid_argument("file / record size mismatch");
+  }
+
+  return file_size / record_size;
+}
+
 int compress() {
-  std::ifstream is(
-    "../../data/layer_states/board_size-3.max_exponent-10.max_depth-0/1438.bin",
-    std::ios::in | std::ios::binary);
-  std::vector<uint64_t> values;
-  std::vector<uint8_t> out;
+  std::ifstream is(BIN_IN_FILE, std::ios::in | std::ios::binary);
+  std::ofstream os(VBYTE_FILE, std::ios::out | std::ios::binary);
+  uint8_t buf[16];
+  uint64_t previous = 0;
+
+  size_t num_values = count_records_in_file(BIN_IN_FILE, 8);
+  os.write(reinterpret_cast<const char*>(&num_values), sizeof(num_values));
+
   for (;;) {
     uint64_t value;
     is.read(reinterpret_cast<char *>(&value), sizeof(value));
     if (!is) break;
-    values.push_back(value);
-  }
 
-  size_t num_values = values.size();
-  std::cout << num_values << std::endl;
+    size_t bytes_out = vbyte_append_sorted64(buf, previous, value);
+    os.write(reinterpret_cast<const char*>(buf), bytes_out);
+    if (!os) {
+      std::cout << "compress: write failed" << std::endl;
+      return 1;
+    }
 
-  std::cout << "comp 0" << std::endl;
-  out.resize(num_values * sizeof(uint64_t));
-  std::cout << "comp 1" << std::endl;
-  size_t num_bytes_out = vbyte_compress_sorted64(
-    values.data(), out.data(), 0, num_values);
-
-  std::ofstream os(
-    "../../data/layer_states/board_size-3.max_exponent-10.max_depth-0/1438.vbyte",
-    std::ios::out | std::ios::binary);
-
-  std::cout << "comp 2" << std::endl;
-  os.write(reinterpret_cast<const char*>(&num_values), sizeof(num_values));
-
-  std::cout << "comp 3" << std::endl;
-  for (size_t i = 0; i < num_bytes_out; ++i) {
-    os.write(reinterpret_cast<const char*>(out.data() + i), 1);
-    if (!os) return 1;
+    previous = value;
   }
   return 0;
 }
 
 int decompress() {
   std::cout << "decomp" << std::endl;
-  std::ifstream is(
-    "../../data/layer_states/board_size-3.max_exponent-10.max_depth-0/1438.vbyte",
-    std::ios::in | std::ios::binary);
+  std::ifstream is(VBYTE_FILE, std::ios::in | std::ios::binary);
   std::vector<uint8_t> in;
   std::vector<uint64_t> values;
 
@@ -68,10 +73,7 @@ int decompress() {
 
   std::cout << "processed " << num_bytes_in << std::endl;
 
-  std::ofstream os(
-    "../../data/layer_states/board_size-3.max_exponent-10.max_depth-0/1438.vbyte.bin",
-    std::ios::out | std::ios::binary);
-
+  std::ofstream os(BIN_OUT_FILE, std::ios::out | std::ios::binary);
   for (size_t i = 0; i < num_values; ++i) {
     os.write(reinterpret_cast<const char*>(values.data() + i), sizeof(uint64_t));
     if (!os) return 1;
@@ -80,5 +82,6 @@ int decompress() {
 }
 
 int main() {
-  return compress() || decompress();
+  return compress();
+  // return compress() || decompress();
 }
