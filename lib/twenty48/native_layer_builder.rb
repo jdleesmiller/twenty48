2bc82eb6eee51d052e3fae0922bf7a968ff768fb
John Lees-Miller <jdleesmiller@gmail.com>
2017-02-20 22:54:06 +0000

    Fix argument count error

--- lib/twenty48/native_layer_builder.rb
+++ lib/twenty48/native_layer_builder.rb
@@ -7,14 +7,14 @@ module Twenty48
   # Common methods for the native LayerBuilder class.
   #
   module NativeLayerBuilder
-    def self.create(board_size, resolver)
+    def self.create(board_size, *args)
       klass = case board_size
               when 2 then LayerBuilder2
               when 3 then LayerBuilder3
               when 4 then LayerBuilder4
               else raise "bad layer builder board_size: #{board_size}"
               end
-      klass.new(resolver)
+      klass.new(*args)
     end
   end
 
