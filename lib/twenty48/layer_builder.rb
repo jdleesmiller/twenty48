a1c239ae8c5a7d5b48ef33e584e209fd98166e9f
John Lees-Miller <jdleesmiller@gmail.com>
2017-02-23 21:41:01 +0000

    Clean up long methods

--- lib/twenty48/layer_builder.rb
+++ lib/twenty48/layer_builder.rb
@@ -57,9 +57,8 @@ module Twenty48
 
       layer_sum = 4
       while layer_sum <= 8
-        layer_start_states = start_states.select do |state|
-          state.sum == layer_sum
-        end.sort
+        layer_start_states =
+          start_states.select { |state| state.sum == layer_sum }.sort
 
         layer_start_states_by_max_value =
           layer_start_states.group_by(&:max_value)
@@ -113,7 +112,7 @@ module Twenty48
     #
     def prepare_to_restart_from(layer_sum)
       to_fragment = LayerPartName.glob(layer_folder).select do |name|
-        name.sum > layer_sum 
+        name.sum > layer_sum
       end
 
       to_fragment.each do |name|
@@ -156,22 +155,13 @@ module Twenty48
       # when we try to merge.
       raise "too many batches: #{num_batches}" if num_batches > 1000
 
-      Parallel.each(0...num_batches) do |batch|
-        index_entry = input_index[batch]
-        vbyte_reader = VByteReader.new(input_pathname,
-          index_entry.byte_offset, index_entry.previous, input_batch_size)
-        NativeLayerBuilder.create(
-          board_size, vbyte_reader, max_value,
-          layer_fragment_pathname(sum, max_value, 1, 0, batch),
-          layer_fragment_pathname(sum, max_value, 1, 1, batch),
-          layer_fragment_pathname(sum, max_value, 2, 0, batch),
-          layer_fragment_pathname(sum, max_value, 2, 1, batch),
-          valuer
-        )
-        STDOUT.write('.') if @verbose
-        GC.start
+      batches = Array.new(num_batches) do |i|
+        [i, input_index[i].byte_offset, input_index[i].previous]
       end
-      puts if @verbose # put a line break after the dots from the parts
+
+      build_layer_part_batches(
+        sum, max_value, input_pathname, input_batch_size, batches
+      )
     end
 
     def count_states(sum, max_value)
@@ -226,6 +216,26 @@ module Twenty48
       ).in(layer_folder)
     end
 
+    def build_layer_part_batches(
+      sum, max_value, input_pathname, input_batch_size, batches
+    )
+      Parallel.each(batches) do |index, offset, previous|
+        vbyte_reader = VByteReader.new(input_pathname, offset, previous,
+          input_batch_size)
+        NativeLayerBuilder.create(
+          board_size, vbyte_reader, max_value,
+          layer_fragment_pathname(sum, max_value, 1, 0, index),
+          layer_fragment_pathname(sum, max_value, 1, 1, index),
+          layer_fragment_pathname(sum, max_value, 2, 0, index),
+          layer_fragment_pathname(sum, max_value, 2, 1, index),
+          valuer
+        )
+        STDOUT.write('.') if @verbose
+        GC.start
+      end
+      puts if @verbose # put a line break after the dots from the parts
+    end
+
     def reduce_layer_parts(sum, max_value)
       # The max_values are processed in ascending order, so once we've built
       # successors from a part, all parts with layer_sum = this layer_sum + 2
@@ -243,21 +253,25 @@ module Twenty48
       end
 
       fragments_by_output_part.each do |output_name, input_names|
-        # Can't currently handle too many; we may run out of file descriptors
-        # when we try to merge.
-        raise "too many batches: #{num_batches}" if input_names.size > 1000
-        input_pathnames = input_names.map { |name| name.in(layer_folder) }
-        log_reduce_step(output_name.sum, output_name.max_value, input_pathnames)
-
-        output_pathname = output_name.in(layer_folder)
-        raise "already done: #{output_pathname}" if File.exist?(output_pathname)
-
-        num_states, vbyte_index = merge_files(input_pathnames, output_pathname)
-        write_layer_part_info(output_name.sum, output_name.max_value,
-          num_states: num_states, index: vbyte_index)
-
-        FileUtils.rm input_pathnames
+        reduce_output_part_fragments(output_name, input_names)
       end
+    end
+
+    def reduce_output_part_fragments(output_name, input_names)
+      # Can't currently handle too many; we may run out of file descriptors
+      # when we try to merge.
+      raise "too many batches: #{num_batches}" if input_names.size > 1000
+      input_pathnames = input_names.map { |name| name.in(layer_folder) }
+      log_reduce_step(output_name.sum, output_name.max_value, input_pathnames)
+
+      output_pathname = output_name.in(layer_folder)
+      raise "already done: #{output_pathname}" if File.exist?(output_pathname)
+
+      num_states, vbyte_index = merge_files(input_pathnames, output_pathname)
+      write_layer_part_info(output_name.sum, output_name.max_value,
+        num_states: num_states, index: vbyte_index)
+
+      FileUtils.rm input_pathnames
     end
 
     def merge_files(input_files, output_file)
