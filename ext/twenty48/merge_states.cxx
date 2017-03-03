cd51c61b50a4f3b10010c4ec90f771b264a6a9fe
John Lees-Miller <jdleesmiller@gmail.com>
2017-03-03 21:48:56 +0000

    Use a priority queue in the merge step

--- ext/twenty48/merge_states.cxx
+++ ext/twenty48/merge_states.cxx
@@ -1,84 +1,99 @@
 #include "merge_states.hpp"
 
 #include <limits>
-#include <memory>
+#include <queue>
 
 #include "vbyte_reader.hpp"
 #include "vbyte_writer.hpp"
 
 namespace twenty48 {
 
+//
+// A wrapper for a vbyte_reader_t that just remembers the last value that we
+// read. We need to remember the value so we can put it into the appropriate
+// place in the priority queue.
+//
+struct head_t {
+  head_t(const char *input_pathname) : reader(input_pathname) { read(); }
+
+  uint64_t peek() const {
+    return value;
+  }
+
+  void read() {
+    value = reader.read();
+  }
+
+private:
+  vbyte_reader_t reader;
+  uint64_t value;
+};
+
+typedef head_t *head_ptr;
+
+struct head_compare_t {
+  bool operator()(const head_ptr &lhs, const head_ptr &rhs) const {
+    return lhs->peek() > rhs->peek();
+  }
+};
+
+typedef std::priority_queue<
+  head_ptr, std::vector<head_ptr>, head_compare_t> head_queue_t;
+
 size_t merge_states(const std::vector<std::string> &input_pathnames,
   const char *output_pathname, size_t index_stride,
   twenty48::vbyte_index_t &vbyte_index)
 {
   size_t num_states = 0;
-  const size_t n = input_pathnames.size();
-  const uint64_t inf_value = std::numeric_limits<uint64_t>::max();
-  typedef std::vector<std::unique_ptr<vbyte_reader_t> > reader_vector_t;
+  uint64_t value = 0;
 
   vbyte_writer_t vbyte_writer(output_pathname);
 
   // Open input files.
-  reader_vector_t inputs;
+  // Invariant: the inputs queue contains only heads for which the value is
+  // non-zero; a zero value indicates end of file, so this means that we are
+  // removing heads that reach end of file.
+  head_queue_t inputs;
   for (typename std::vector<std::string>::const_iterator it =
     input_pathnames.begin(); it != input_pathnames.end(); ++it) {
-    inputs.emplace_back(new vbyte_reader_t(it->c_str()));
-  }
-
-  // Read first value from each file.
-  std::vector<uint64_t> heads;
-  for (typename reader_vector_t::iterator it = inputs.begin();
-    it != inputs.end(); ++it) {
-    uint64_t nybbles = (*it)->read();
-    if (nybbles == 0) {
-      heads.push_back(inf_value);
+    head_ptr head = new head_t(it->c_str());
+    if (head->peek() > 0) {
+      inputs.push(head);
     } else {
-      heads.push_back(nybbles);
+      delete head;
     }
   }
 
-  std::vector<size_t> min_indexes;
-  for (;;) {
-    // Find the smallest state among the current heads.
-    uint64_t min_value = inf_value;
-    for (size_t i = 0; i < n; ++i) {
-      if (heads[i] < min_value) {
-        min_value = heads[i];
-        min_indexes.clear();
-        min_indexes.push_back(i);
-      } else if (heads[i] == min_value) {
-        min_indexes.push_back(i);
+  while (inputs.size() > 0) {
+    head_ptr top = inputs.top();
+    inputs.pop();
+
+    // We may pop the same state off multiple streams; only write it once.
+    if (top->peek() != value) {
+      value = top->peek();
+
+      // Write the min state.
+      vbyte_writer.write(value);
+      num_states += 1;
+
+      // Update index if necessary.
+      if (num_states % index_stride == 0) {
+        vbyte_index.push_back(vbyte_index_entry_t(
+          vbyte_writer.get_bytes_written(),
+          vbyte_writer.get_previous()));
       }
     }
 
-    // If all heads are infinite, we're done.
-    if (min_value == inf_value) break;
+    top->read();
 
-    // Write the min state.
-    vbyte_writer.write(min_value);
-    num_states += 1;
-
-    // Update index if necessary.
-    if (num_states % index_stride == 0) {
-      vbyte_index.push_back(vbyte_index_entry_t(
-        vbyte_writer.get_bytes_written(),
-        vbyte_writer.get_previous()));
+    // A zero means that we've reached the end of the input; remove it.
+    if (top->peek() == 0) {
+      delete top;
+    } else {
+      inputs.push(top);
     }
-
-    // Pop the head states that matched the min state we just wrote.
-    for (typename std::vector<size_t>::const_iterator it =
-      min_indexes.begin(); it != min_indexes.end(); ++it) {
-      uint64_t next_nybbles = inputs[*it]->read();
-      if (next_nybbles == 0) {
-        heads[*it] = inf_value;
-      } else {
-        heads[*it] = next_nybbles;
-      }
-    }
-
-    min_indexes.clear();
   }
+
   return num_states;
 }
 
