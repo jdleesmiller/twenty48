d598cef955d6ab478d2cd6aeefae5cc8c0ee6ea3
John Lees-Miller <jdleesmiller@gmail.com>
2017-08-10 18:19:47 -0600

    Tidy up the binomial mixture model

--- data/markov_chain/plot.R
+++ data/markov_chain/plot.R
@@ -87,205 +87,88 @@ sumProbabilities
 # check that probabilities sum to 1
 aggregate(probability ~ max_exponent, sumProbabilities, sum)
 
-# want: probability of reaching the given sum x exactly in units of 2 or 4 in
-# n moves: f((x - n)/2, n/2, p) for f = dbinom
-# we reach x in n moves if we have x - n successes, where a success is a 4
-
-# mixture = max_exponent, num_moves, sum, probability
-makeBinomialMixture <- function () {
+#
+# Create the joint distribution of the number of moves and the sum of the values
+# after that number of moves. This is binomial:
+#
+# P(M = m, S = s) = B(s / 2 - m, m, p)
+#
+# Since, in m moves, we accumulate 2m for sure, plus 2 for each success (i.e.
+# we get a 4). That is, the sum is 2m + 2k, so if k = s/2 - m, the sum is
+# 2m + s - 2m = s.
+#
+makeBinomialJoint <- function () {
   p <- 0.1
-  indices <- with(sumProbabilities, list(max_exponent, state_sum))
-  do.call(rbind, by(sumProbabilities, indices, function (part) {
-    maxExponent <- part$max_exponent[1]
-    stateSum <- part$state_sum[1]
-    maxMoves <- stateSum / 2
-    minMoves <- ceiling(maxMoves / 2)
-    # print(paste(minMoves, maxMoves))
-    # 1024 in 1024 moves -> 1024 moves, 0 successes
-    # 1024 in 1023 moves -> 1023 moves, 1 success
-    # ...
-    # 1024 in 512 moves -> 512 moves, 512 successes
-    #
-    # 1033 in 1033 moves -> 0 successes
-    # 1033 in 1032 moves -> 1 success
-    # ...
-    # 1033 in 517 moves -> 516 successes
-    #
-    # hit 2066 in say 950 moves
-    # means we need (2066-2*950) = 166 in extras, so 83 4s.
-    # 1033-950 = 83
-    #
-    # or 949 moves -> 1898 in 2s; and 83 4s gets you to 2064, then one more 4
-    # gets you over 2066. However... doesn't it mean that you get to 2068? There
-    # is another group for that.
-    #
-    # 1033 - 950 = 83 successes in 949 moves -> 866 failures
-    # (n C x-n) p^(x-n) q^n + (n-1 C x-n) p^(x-n) q^(n-1) p
-    #
-    n <- seq(minMoves, maxMoves)
-    data.frame(
-      max_exponent = maxExponent,
-      state_sum = stateSum,
-      moves = n,
-      probability = dbinom(maxMoves - n, n, p) +
-        dbinom(maxMoves - n, n - 1, p) * p
-    )
+  do.call(rbind, lapply(3:11, function (maxExponent) {
+    stateSums <- subset(sumProbabilities, max_exponent == maxExponent)$state_sum
+    do.call(rbind, lapply(stateSums, function (stateSum) {
+      maxMoves <- stateSum / 2
+      minMoves <- ceiling(maxMoves / 2)
+      numMoves <- seq(minMoves, maxMoves)
+      data.frame(
+        max_exponent = maxExponent,
+        num_moves = numMoves,
+        state_sum = stateSum,
+        probability = dbinom(stateSum / 2 - numMoves, numMoves, p)
+      )
+    }))
   }))
 }
-binomialMixture <- makeBinomialMixture()
+binomialJoint <- makeBinomialJoint()
+head(binomialJoint)
+
+#
+# We want P(M = m | S = s), which we can find from the joint distribution and
+# the marginal distribution of S by
+#
+# P(M = m | S = s) = P(M = m, S = s) / P(S = s)
+#
+
+binomialStateSumMargin <- aggregate(
+  probability ~ max_exponent + state_sum,
+  binomialJoint,
+  sum)
+
+binomialMoveGivenSum <- transform(
+  merge(binomialJoint, binomialStateSumMargin,
+    by = c('max_exponent', 'state_sum'),
+    suffixes = c('_joint', '_state_sum_margin')),
+  probability_moves_given_sum = probability_joint / probability_state_sum_margin
+)[, c('max_exponent', 'state_sum', 'num_moves', 'probability_moves_given_sum')]
+head(binomialMoveGivenSum)
+
+#
+# Finally, if we let W be the event that we've won, we have P(S = s | W) from
+# the absorption probabilities of the Markov chain.
+#
+# Want P(M = m, S = s | W) = P(M = m | S = s, W) * P(S = s | W)
+# P(MS|W) = P(M|SW)P(S|W)
+# P(M|SW) = P(M|S) because M and W are conditionally independent given S.
+#
+# Is an absorbing probability P(S|W)? We know we are going to be absorbed, so
+# it's just a question of which state. We've then aggregated up by sum, so I
+# think yes, P(S|W) is an absorbing probability.
 
 weightedMixture <- transform(
   merge(
-    binomialMixture, sumProbabilities,
-    by = c('max_exponent', 'state_sum'),
-    suffixes = c('_binomial', '_sum')),
-  density = probability_binomial * probability_sum
-)
+    binomialMoveGivenSum, sumProbabilities,
+    by = c('max_exponent', 'state_sum')),
+  density = probability_moves_given_sum * probability
+)[, c('max_exponent', 'state_sum', 'num_moves', 'density')]
 head(weightedMixture)
 
-plotMixture <- function (maxExponent) {
+local({
+  maxExponent <- 3
   ggplot(
     subset(weightedMixture, max_exponent == maxExponent & density > 1e-6),
-    aes(y = density)) +
+    aes(num_moves, density)) +
     geom_bar(
       aes(x = moves),
       data = subset(movesHistogram, max_exponent == maxExponent),
       stat = 'identity') +
     geom_area(
-      aes(x = moves - 2, color = factor(state_sum), fill = factor(state_sum)),
+      aes(x = num_moves - 2,
+        color = factor(state_sum),
+        fill = factor(state_sum)),
       position = 'stack', alpha = 0.5)
-}
-plotMixture(11)
-
-# Check mixture stats
-
-mixtureMeanVariance <- local({
-  mixture <- aggregate(density ~ moves + max_exponent, weightedMixture, sum)
-  mixtureMean <-
-    aggregate(density * moves ~ max_exponent, mixture, sum)
-  names(mixtureMean) <- c('max_exponent', 'moves_mean')
-  mixtureVariance <- aggregate(
-    density * (moves - moves_mean)^2 ~ max_exponent,
-    merge(mixture, mixtureMean, by = 'max_exponent'),
-    sum
-  )
-  names(mixtureVariance) <- c('max_exponent', 'moves_variance')
-  merge(mixtureMean, mixtureVariance)
 })
-mixtureMeanVariance
-
-# mixture = max_exponent, num_moves, sum, probability
-makeBinomialMixture2 <- function () {
-  p <- 0.1
-  # idea: compute binomials for fixed numbers of moves, then sum them up
-  maxExponent <- 4
-  maxMoves <- 2**(maxExponent)
-  numMoves <- seq(0, maxMoves)
-  params <- subset(
-    data.frame(
-      successes = numMoves,
-      num_moves = rep(numMoves, each = maxMoves)
-    ),
-    successes < num_moves)
-  result <- aggregate(
-    density ~ max_exponent + state_sum + num_moves,
-    transform(params,
-      density = dbinom(successes, num_moves, p),
-      max_exponent = maxExponent,
-      state_sum = 2 * num_moves + 2 * successes),
-    sum)
-  result[with(result, order(max_exponent, state_sum, num_moves)),]
-}
-binomialMixture2 <- makeBinomialMixture2()
-head(binomialMixture2)
-
-ggplot(
-  aggregate(
-    density ~ max_exponent + state_sum,
-    binomialMixture2,
-    sum
-  ),
-  aes(state_sum, density)) + geom_line()
-
-ggplot(
-  aggregate(
-    density ~ max_exponent + num_moves,
-    binomialMixture2,
-    sum
-  ),
-  aes(num_moves, density)) + geom_line()
-
-# Still not getting all the mass...
-# I think the reason is that there's always a chance that you miss the number.
-# To hit it, you need to be on state n-2 and get a 2 (probability 0.9), or
-# you need to be on state n-4 and get a 4 (probability 0.1).
-# The absorption probabilities from the Markov chain should already have this
-# effect built in. But there's still no way to recover the mass.
-
-weightedMixture2 <- transform(
-  merge(
-    binomialMixture2,
-    subset(sumProbabilities, max_exponent == 11),
-    by = c('max_exponent', 'state_sum')
-  ),
-  joint_density = density * probability
-)
-
-ggplot(subset(aggregate(
-  density ~ state_sum,
-  binomialMixture2,
-  sum), density > 1e-6),
-  aes(state_sum, density)) + geom_line()
-
-ggplot(
-  subset(weightedMixture2, joint_density > 1e-6),
-  aes(num_moves, joint_density * 1.1)) +
-  geom_bar(
-    aes(x = moves, y = density),
-    data = subset(movesHistogram, max_exponent == 11),
-    stat = 'identity') +
-  geom_area(
-    aes(x = num_moves - 2, color = factor(state_sum), fill = factor(state_sum)),
-    position = 'stack', alpha = 0.5)
-
-
-    # mixture = max_exponent, num_moves, sum, probability
-
-makeBinomialMixture3 <- function () {
-  p <- 0.1
-  maxExponent <- 11
-  stateSums <- subset(sumProbabilities, max_exponent == maxExponent)$state_sum
-  do.call(rbind, lapply(stateSums, function (stateSum) {
-    maxMoves <- stateSum / 2
-    minMoves <- ceiling(maxMoves / 2)
-    numMoves <- seq(minMoves, maxMoves)
-    data.frame(
-      max_exponent = maxExponent,
-      num_moves = numMoves,
-      state_sum = stateSum,
-      density = dbinom(stateSum / 2 - numMoves, numMoves, p)
-    )
-  }))
-}
-binomialMixture3 <- makeBinomialMixture3()
-head(binomialMixture3)
-
-weightedMixture3 <- transform(
-  merge(
-    binomialMixture3,
-    subset(sumProbabilities, max_exponent == 11),
-    by = c('max_exponent', 'state_sum')
-  ),
-  joint_density = density * probability
-)
-
-ggplot(
-  subset(weightedMixture3, joint_density > 1e-6),
-  aes(num_moves, joint_density * 1.1)) +
-  geom_bar(
-    aes(x = moves, y = density),
-    data = subset(movesHistogram, max_exponent == 11),
-    stat = 'identity') +
-  geom_area(
-    aes(x = num_moves - 2, color = factor(state_sum), fill = factor(state_sum)),
-    position = 'stack', alpha = 0.5)
